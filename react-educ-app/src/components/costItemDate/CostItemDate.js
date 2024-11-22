2619a174748b9e529000e32b29ea02b076cb8e33
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2024-11-22 22:13:18 +0700

    add css

--- react-educ-app/src/components/costItemDate/CostItemDate.js
+++ react-educ-app/src/components/costItemDate/CostItemDate.js
@@ -1,9 +1,10 @@
+import "./CostItemDate.css";
 function CostItemDate(props) {
   const month = props.date.toLocaleString("ru-RU", { month: "long" });
   const day = props.date.toLocaleString("ru-RU", { day: "2-digit" });
   const year = props.date.getFullYear();
   return (
-    <div>
+    <div className="cost-item-date">
       <div>{day}</div>
       <div>{month}</div>
       <div>{year}</div>
