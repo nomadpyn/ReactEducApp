3ee5a9e167aef8414efd0e3d396f3039eaa0f408
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2024-11-22 22:02:36 +0700

    create CostItemDate

---
+++ react-educ-app/src/components/costItemDate/CostItemDate.js
@@ -0,0 +1,14 @@
+function CostItemDate(props) {
+  const month = props.date.toLocaleString("ru-RU", { month: "long" });
+  const day = props.date.toLocaleString("ru-RU", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div>
+      <div>{day}</div>
+      <div>{month}</div>
+      <div>{year}</div>
+    </div>
+  );
+}
+
+export default CostItemDate;
