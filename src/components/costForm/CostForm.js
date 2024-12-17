b84ec96532dd90441795779b7115f0998c93b478
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2024-12-17 22:00:54 +0700

    add handlers

--- src/components/costForm/CostForm.js
+++ src/components/costForm/CostForm.js
@@ -1,20 +1,48 @@
-import './CostForm.css';
+import { useState } from "react";
+import "./CostForm.css";
 
 const CostForm = () => {
+  const [userInput, setUserInput] = useState({
+    name: "",
+    amount: "",
+    date: "",
+  });
+
+  const nameChangeHandler = (e) => {
+    setUserInput({
+        ...userInput,   
+        name: e.target.value
+    })
+  }
+
+  const amountChangeHandler = (e) => {
+    setUserInput({
+        ...userInput,
+        amount: e.target.value
+    })
+  }
+
+  const dateChangeHandler = (e) => {
+    setUserInput({
+        ...userInput,
+        date: e.target.value
+    })
+  }
+
   return (
     <form>
       <div className="new-cost-controls">
         <div className="new-cost-control">
           <label>Название</label>
-          <input type="text" />
+          <input type="text" onChange={nameChangeHandler}/>
         </div>
         <div className="new-cost-control">
           <label>Сумма (руб)</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
         </div>
         <div className="new-cost-control">
           <label>Дата</label>
-          <input type="date" min="2024-01-01" />
+          <input type="date" min="2024-01-01" onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className="new-cost-actions">
