954ce9430767aebe57042d0265d39fb754411624
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2024-12-19 22:22:45 +0700

    Create costFilter

--- src/components/costs/Costs.js
+++ src/components/costs/Costs.js
@@ -1,20 +1,34 @@
+import React, { useState } from "react";
 import Card from "../card/Card";
+import CostFilter from "../costFilter/CostFilter";
 import CostItem from "../costItem/CostItem";
 import "./Costs.css";
 
 const Costs = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2021");
+
+  const onSelectChangeHandler = (year) => {
+    setSelectedYear(year);
+  };
+
   return (
-    <Card className="costs">
-      {props.costs.map((cost, key) => (
-        <CostItem
-          key={key}
-          date={cost.date}
-          description={cost.description}
-          amount={cost.amount}
+    <div>
+      <Card className="costs">
+        <CostFilter
+          year={selectedYear}
+          onSelectChange={onSelectChangeHandler}
         />
-      ))}
-    </Card>
+        {props.costs.map((cost, key) => (
+          <CostItem
+            key={key}
+            date={cost.date}
+            description={cost.description}
+            amount={cost.amount}
+          />
+        ))}
+      </Card>
+    </div>
   );
-}
+};
 
 export default Costs;
